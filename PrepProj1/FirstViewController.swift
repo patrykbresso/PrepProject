90b4f6a1cc4c0f848d66c6af5060798d622426ef
Patryk Bresso <patrykbresso@Patryks-MacBook-Pro.local>
2017-05-16 18:42:42 +0200

    Initial Commit

---
+++ PrepProj1/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PrepProj1
+//
+//  Created by Patryk Bresso on 16/05/2017.
+//  Copyright © 2017 Patryk Bresso. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PrepProj1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PrepProj1
+//
+//  Created by Patryk Bresso on 16/05/2017.
+//  Copyright © 2017 Patryk Bresso. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
